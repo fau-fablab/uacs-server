729b07a4eafe0f47423f35cf9bd12b62021347c6
sedrubal <sebastian.endres@online.de>
2015-11-18 17:39:06 +0100

    Changed to sqlite3

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'myApp',
+    'myApp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'myproject',
-		'USER': 'myprojectuser',
-        'PASSWORD': 'fablap',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
