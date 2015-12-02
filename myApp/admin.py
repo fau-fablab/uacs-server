c0c3e81495462786665cccbb5d5789d36e0a8458
Markus Fritscher <gurki@ws01.lab.fablab.uni-erlangen.de>
2015-12-02 12:08:05 +0100

    added fields to the admin interface

--- myApp/admin.py
+++ myApp/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 from .models import fablabUser
 from .models import fablabDevice
 
-admin.site.register(fablabUser)
-admin.site.register(fablabDevice)
+class fablabUserAdmin(admin.ModelAdmin):
+	list_display = ('fauid', 'cardid', 'Name', 'Betreuer')
 
+class fablabDeviceAdmin(admin.ModelAdmin):
+	list_display = ('Name', 'runtime', 'active')
+
+admin.site.register(fablabUser, fablabUserAdmin)
+admin.site.register(fablabDevice, fablabDeviceAdmin)
+
+# taken from http://www.djangobook.com/en/2.0/chapter06.html	
 
 # Register your models here.
