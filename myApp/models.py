1cff1ff9b4a3c935c1ceb719c51145961efd86fa
Markus Fritscher <gurki@ws01.lab.fablab.uni-erlangen.de>
2015-12-02 17:51:42 +0100

    addded default values

--- myApp/models.py
+++ myApp/models.py
@@ -6,11 +6,11 @@ class fablabUser(models.Model):
     fauid = models.CharField(max_length=10, default="faudauid")
     cardid = models.CharField(max_length=15, default="00000")
     Name = models.CharField(max_length=200, default='fablabdau')
-    Betreuer = models.BooleanField()
-    Fraese = models.BooleanField()
-    Laser = models.BooleanField()
-    Fernbedienung = models.BooleanField()
-    Kabelbinder = models.BooleanField()
+    Betreuer = models.BooleanField(default = False)
+    Fraese = models.BooleanField(default = False)
+    Laser = models.BooleanField(default = False)
+    Fernbedienung = models.BooleanField(default = False)
+    Kabelbinder = models.BooleanField(default = False)
 
     def __str__(self):
         return str(self.id)
